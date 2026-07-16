b2786cb2d80ec552a74994c01a9e95a9fdc3b04f
NovaFoundSaturn <NovaFoundMars@pm.me>
2026-07-15 20:02:58 -0400

    Added helix support, changed location of the ln in the file.

--- install.sh
+++ install.sh
@@ -10,13 +10,6 @@ rm ~/.config/fish/config.fish
 
 mv ./dotfiles ~/Projects/
 
-# Update and Install new packages
-sudo pacman -Syu niri noctalia sddm nautilus ghostty ghostty-nautilus zen-browser-bin github-cli gnome-disk-utility adw-gtk-theme brightnessctl capitaine-cursors xwayland-satellite cachyos-gaming-meta steam discord 
-###!!! THERE IS CURRENTLY SOMETHING IN THE CACHYOS NOCTALIA NIRI PACKAGE THAT FIXES DISCORD STREAMING, Idk what does it exactly
-
-# Enable display manager
-sudo systemctl enable sddm
-
 ln -s ~/Projects/dotfiles/.vimrc ~/
 ln -s ~/Projects/dotfiles/fastfetch ~/.config
 ln -s ~/Projects/dotfiles/niri ~/.config
@@ -24,3 +17,11 @@ ln -s ~/Projects/dotfiles/noctalia ~/.config
 ln -s ~/Projects/dotfiles/fish/config.fish ~/.config/fish/
 ln -s ~/Projects/dotfiles/ghostty ~/.config
 ln -s ~/Projects/dotfiles/wallpapers ~/Pictures/Wallpapers
+ln -s ~/Projects/dotfiles/helix ~/.config/
+
+# Update and Install new packages
+sudo pacman -Syu niri noctalia sddm nautilus ghostty ghostty-nautilus zen-browser-bin github-cli gnome-disk-utility adw-gtk-theme brightnessctl capitaine-cursors xwayland-satellite cachyos-gaming-meta steam discord helix
+###!!! THERE IS CURRENTLY SOMETHING IN THE CACHYOS NOCTALIA NIRI PACKAGE THAT FIXES DISCORD STREAMING, Idk what does it exactly
+
+# Enable display manager
+sudo systemctl enable sddm
