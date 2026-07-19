7b0d97d343dd139b0facb345eb571b997467bdc6
NovaFoundSaturn <NovaFoundMars@pm.me>
2026-07-18 23:58:59 -0400

    added a pkgs variable

--- install.sh
+++ install.sh
@@ -2,6 +2,30 @@
 
 # A simple shell script to install my dotfiles and setup some basics for my system.
 
+set pkgs \
+niri \
+noctalia \
+sddm \
+nautilus \
+ghostty \
+ghostty-nautilus \
+zen-browser-bin \
+github-cli \
+github-desktop \
+gnome-disk-utility \
+adw-gtk-theme \
+brightnessctl \
+capitaine-cursors \
+xwayland-satellite \
+cachyos-gaming-meta \
+steam \
+discord \
+helix \
+libreoffice-fresh \
+gnome-calculator \
+proton-vpn-gtk-app 
+###!!! THERE IS CURRENTLY SOMETHING IN THE CACHYOS NOCTALIA NIRI PACKAGE THAT FIXES DISCORD STREAMING, Idk what does it exactly
+
 git clone https://github.com/NovaFoundSaturn/dotfiles.git
 
 mkdir ~/Projects
@@ -20,8 +44,7 @@ ln -s ~/Projects/dotfiles/wallpapers ~/Pictures/Wallpapers
 ln -s ~/Projects/dotfiles/helix ~/.config/
 
 # Update and Install new packages
-sudo pacman -Syu niri noctalia sddm nautilus ghostty ghostty-nautilus zen-browser-bin github-cli gnome-disk-utility adw-gtk-theme brightnessctl capitaine-cursors xwayland-satellite cachyos-gaming-meta steam discord helix libreoffice-fresh gnome-calculator proton-vpn-gtp-app github-desktop
-###!!! THERE IS CURRENTLY SOMETHING IN THE CACHYOS NOCTALIA NIRI PACKAGE THAT FIXES DISCORD STREAMING, Idk what does it exactly
+sudo pacman -Syu $pkgs
 
 # Enable display manager
 sudo systemctl enable sddm
